43da30da3c0e1fd69061f00a215605e6d2eef6f7
Manuel <manudzmz@gmail.com>
2016-09-23 02:03:55 +0200

    Añadido nombre a las URLs

--- post/urls.py
+++ post/urls.py
@@ -4,9 +4,8 @@ from django.contrib import admin
 from post.views import HomeView, PostDetailView, UserPostsView, CreatePostView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', HomeView.as_view()),
-    url(r'^blogs/(?P<blogger>[a-z0-9_-]+)/$', UserPostsView.as_view()),
-    url(r'^blogs/[a-z0-9_-]+/(?P<pk>[0-9]+)$', PostDetailView.as_view()),
-    url(r'^new-post$', CreatePostView.as_view()),
+    url(r'^$', HomeView.as_view(), name='post_home'),
+    url(r'^blogs/(?P<blogger>[a-z0-9_-]+)/$', UserPostsView.as_view(), name='post_usersposts'),
+    url(r'^blogs/[a-z0-9_-]+/(?P<pk>[0-9]+)$', PostDetailView.as_view(), name='post_postdetail'),
+    url(r'^new-post$', CreatePostView.as_view(), name='post_createpost'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from users.views import LoginView, LogoutView, BlogsView
 
 urlpatterns = [
-    url(r'^login$', LoginView.as_view()),
-    url(r'^logout$', LogoutView.as_view()),
-    url(r'^blogs/$', BlogsView.as_view()),
+    url(r'^login$', LoginView.as_view(), name='users_login'),
+    url(r'^logout$', LogoutView.as_view(), name='users_logout'),
+    url(r'^blogs/$', BlogsView.as_view(), name='users_blogs'),
 ]
