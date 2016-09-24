3cd921a823b5bca836fbdb990fdbb7ebfebddac0
Manuel <manudzmz@gmail.com>
2016-09-24 14:24:30 +0200

    Cambiada herencia de View a APIView

--- users/api.py
+++ users/api.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.views import View
 from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from users.serializers import BlogsSerializer
 
 
-class BlogsListAPI(View):
+class BlogsListAPI(APIView):
     """
     Endpoint que muestra el listado de blogs de la plataforma
     """
@@ -14,6 +14,4 @@ class BlogsListAPI(View):
     def get(self, request):
         users = User.objects.all()
         serializer = BlogsSerializer(users, many=True)
-        renderer = JSONRenderer()
-        data = renderer.render(serializer.data)
-        return HttpResponse(data)
+        return Response(serializer.data)
